939884fbd4e138b08a73807e2be7de65a2be2cc5
VPresich <valya.alexandrova.presich@gmail.com>
2025-02-08 00:14:17 +0100

    changed setupSession

--- src/controllers/users/setupSession.js
+++ src/controllers/users/setupSession.js
@@ -1,16 +1,20 @@
 import { REFRESH_TOKEN_LIFETIME } from '../../constants/index.js';
+import { env } from '../../utils/env.js';
 
 export const setupSession = (res, session) => {
+  const isModeProduction = env('NODE_ENV_PRODUCTION') === 'true';
+
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
-    secure: true,
-    sameSite: 'None',
+    secure: isModeProduction,
+    sameSite: isModeProduction ? 'None' : 'Lax',
     expires: new Date(Date.now() + REFRESH_TOKEN_LIFETIME),
   });
+
   res.cookie('sessionId', session._id, {
     httpOnly: true,
-    secure: true,
-    sameSite: 'None',
+    secure: isModeProduction,
+    sameSite: isModeProduction ? 'None' : 'Lax',
     expires: new Date(Date.now() + REFRESH_TOKEN_LIFETIME),
   });
 };
