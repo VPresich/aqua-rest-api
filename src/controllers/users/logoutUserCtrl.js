1dcf01f684bdb1d9c887dc3422a219d5308d8eb0
VPresich <valya.alexandrova.presich@gmail.com>
2025-02-07 22:32:10 +0100

    Update logoutUserCtrl.js

--- src/controllers/users/logoutUserCtrl.js
+++ src/controllers/users/logoutUserCtrl.js
@@ -1,6 +1,7 @@
 import { logoutUser } from '../../services/users/logoutUser.js';
 
 export const logoutUserCtrl = async (req, res) => {
+  console.log('COOKIES', req.cookies.sessionId);
   if (req.cookies.sessionId) {
     await logoutUser(req.cookies.sessionId);
   }
