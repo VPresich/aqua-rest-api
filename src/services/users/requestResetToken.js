e0b3d4ef32e835d0ff5fc5d9c47c8e9655c3f84c
VPresich <valya.alexandrova.presich@gmail.com>
2025-10-16 14:33:12 +0200

    Added logs for testing email sending

--- src/services/users/requestResetToken.js
+++ src/services/users/requestResetToken.js
@@ -1,19 +1,22 @@
 import createHttpError from 'http-errors';
-import jwt from 'jsonwebtoken';
-import fs from 'node:fs/promises';
-import path from 'node:path';
-import handlebars from 'handlebars';
-import { TEMPLATES_DIR } from '../../constants/index.js';
-import { SMTP } from '../../constants/index.js';
-import { env } from '../../utils/env.js';
 import { UsersCollection } from '../../db/models/user.js';
+import jwt from 'jsonwebtoken';
+import { env } from '../../utils/env.js';
+import { TEMPLATES_DIR } from '../../constants/index.js';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import handlebars from 'handlebars';
+import { SMTP } from '../../constants/index.js';
 import { sendEmail } from '../../utils/sendMail.js';
 
 export const requestResetToken = async (email) => {
+  console.log(' [1] Начало requestResetToken, email:', email);
+
   const user = await UsersCollection.findOne({ email });
   if (!user) {
     throw createHttpError(404, 'User not found!');
   }
+  console.log('EMAIL:', email);
 
   const resetToken = jwt.sign(
     {
@@ -25,21 +28,25 @@ export const requestResetToken = async (email) => {
       expiresIn: '5m',
     },
   );
+  console.log('RESETTOKEN:', resetToken);
 
   const resetPasswordTemplatePath = path.join(
     TEMPLATES_DIR,
     'reset-password-email.html',
   );
+  console.log('TEMPLATEPATH:', resetPasswordTemplatePath);
 
   const templateSource = (
     await fs.readFile(resetPasswordTemplatePath)
   ).toString();
-
   const template = handlebars.compile(templateSource);
+  console.log('TEMPLATE:', template);
+
   const html = template({
     name: user.name,
     link: `${env('APP_DOMAIN')}/password/reset/${resetToken}`,
   });
+  console.log('TEST HTML:', html);
 
   try {
     await sendEmail({
--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,18 +1,30 @@
 import nodemailer from 'nodemailer';
-
+import createHttpError from 'http-errors';
 import { SMTP } from '../constants/index.js';
 import { env } from '../utils/env.js';
 
-const transporter = nodemailer.createTransport({
-  host: env(SMTP.SMTP_HOST),
-  port: Number(env(SMTP.SMTP_PORT)),
-  auth: {
-    user: env(SMTP.SMTP_USER),
-    pass: env(SMTP.SMTP_PASSWORD),
-  },
-  connectionTimeout: 5000,
-});
-
 export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
+  const transporter = nodemailer.createTransport({
+    host: env(SMTP.SMTP_HOST),
+    port: Number(env(SMTP.SMTP_PORT)),
+    auth: {
+      user: env(SMTP.SMTP_USER),
+      pass: env(SMTP.SMTP_PASSWORD),
+    },
+    connectionTimeout: 5000,
+  });
+
+  try {
+    console.log('⏳ Проверяю соединение...');
+    await transporter.verify();
+    console.log('✅ SMTP подключение успешно!');
+  } catch (err) {
+    console.error('Ошибка при проверке SMTP:', err.message);
+    throw createHttpError(500, `Ошибка при проверке SMTP: ${err.message}`);
+  }
+
+  console.log(' Отправка письма...', options);
+
+  await transporter.sendMail(options);
+  console.log('Письмо отправлено:');
 };
